d6e303440cb6dde4528f32ae7a10ad724b96948d
Md Tajuddin <mttajuddin01@gmail.com>
2025-12-30 11:36:17 +0600

    added the all html,css,js file

---
+++ script/main.js
@@ -0,0 +1,24 @@
+/* Header & Home Section Part Start */
+
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["Frontend Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 100,
+        loop: true
+    });
+});
+/* Header & Home Section Part End */
+
+/* Services Section Part Start */
+
+AOS.init({
+    duration: 900,
+    once: true,
+    easing: "ease-in-out"
+});
+
+/* Services Section Part End */
+
+
